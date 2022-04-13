d535b3a6fdd67e980f7ac06c1de521e7827fe4a1
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-13 11:35:05 +0200

    Style the banner component

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -1,26 +1,37 @@
 import React from 'react';
 
+import styles from './Banner.module.css';
+
 const Banner = ({ title, subTitle, imgUrl }) => {
   const handleOnPlay = () => {
     console.log('play button clicked');
   };
 
   return (
-    <>
-      <h2>{title}</h2>
-      <h3>{subTitle}</h3>
-      <button onClick={handleOnPlay}>Play</button>
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+          <h2 className={styles.title}>{title}</h2>
+          <h3 className={styles.subTitle}>{subTitle}</h3>
+
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
       <div
+        className={styles.bannerImg}
         style={{
           backgroundImage: `url(${imgUrl})`,
-          width: '100%',
-          height: '100%',
-          position: 'absolute',
-          backgroundSize: 'cover',
-          backgroundPosition: '50% 50%',
         }}
       ></div>
-    </>
+    </div>
   );
 };
 
