2481b1543d59ca4127ef5e9aaaf7a5441e22bd1e
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-15 11:08:12 +0200

    Style the NavBar component

--- components/navbar/NavBar.js
+++ components/navbar/NavBar.js
@@ -1,24 +1,41 @@
 import React from 'react';
+import Image from 'next/image';
+
+import styles from './NavBar.module.css';
 
 const NavBar = ({ username }) => {
   return (
-    <div>
-      Navbar
-      <p>{username}</p>
-      <ul>
-        <li>Home</li>
-        <li>My List</li>
-      </ul>
-      <nav>
-        <div>
-          <button>
-            <p>{username}</p>
-          </button>
-          <div>
-            <a>Sign Out</a>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink} href="/">
+          <div className={styles.logoWrapper}>
+            <Image
+              src="/static/netflix.svg"
+              alt="Netflix logo"
+              width="128px"
+              height="34px"
+            />
           </div>
-        </div>
-      </nav>
+        </a>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem2}>My List</li>
+        </ul>
+        <nav className={styles.navContainer}>
+          <div>
+            <button className={styles.usernameBtn}>
+              <p className={styles.username}>{username}</p>
+              {/* Expand more icon */}
+            </button>
+            <div className={styles.navDropdown}>
+              <div>
+                <a className={styles.linkName}>Sign Out</a>
+                <div className={styles.lineWrapper}></div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
