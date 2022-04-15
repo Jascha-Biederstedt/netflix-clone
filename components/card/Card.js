a0b49092bced9c20f5bdf8d9b280256e09b2e5c0
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-15 12:26:01 +0200

    Style Card component

--- components/card/Card.js
+++ components/card/Card.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Image from 'next/image';
 
+import styles from './Card.module.css';
+
 const Card = ({ imgUrl, size }) => {
+  const classMap = {
+    large: styles.lgItem,
+    medium: styles.mdItem,
+    small: styles.smItem,
+  };
+
   return (
-    <div>
-      <Image src={imgUrl} alt="image" width="300px" height="300px" />
+    <div className={styles.container}>
+      <div className={classMap[size]}>
+        <Image
+          src={imgUrl}
+          alt="image"
+          layout="fill"
+          className={styles.cardImg}
+        />
+      </div>
     </div>
   );
 };
