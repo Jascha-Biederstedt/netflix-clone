7a743c2eba4dfa7a604c5c1f5d290ab9e8b5a2ab
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-16 09:39:27 +0200

    Set up login page

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Login = () => {
+  return (
+    <div>
+      <Head>
+        <title>Netflix Sign In</title>
+      </Head>
+    </div>
+  );
+};
+
+export default Login;
