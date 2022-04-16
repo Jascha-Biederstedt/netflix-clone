c1d3584e0da010bf42143f134fefda4d7199692d
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-16 10:02:05 +0200

    Add sign-in page structure

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from '../styles/Login.module.css';
 
 const Login = () => {
+  const handleLoginWithEmail = e => {
+    e.preventDeafult();
+    console.log('test');
+  };
+
   return (
     <div>
       <Head>
         <title>Netflix Sign In</title>
       </Head>
+
+      <header>
+        <div className={styles.headerWrapper}>
+          <a className={styles.logoLink} href="/">
+            <div className={styles.logoWrapper}>
+              <Image
+                src="/static/netflix.svg"
+                alt="Netflix logo"
+                width="128px"
+                height="34px"
+              />
+            </div>
+          </a>
+        </div>
+      </header>
+
+      <main className={styles.main}>
+        <div className={styles.mainWrapper}>
+          <h1 className={styles.signinHeader}>Sign In</h1>
+          <input
+            className={styles.emailInput}
+            type="text"
+            placeholder="Email address"
+          />
+          <p className={styles.userMsg}>Enter a valid email address</p>
+          <p>You'll receive a link for passwordless authentication</p>
+          <button className={styles.loginBtn} onClick={handleLoginWithEmail}>
+            Sign In
+          </button>
+        </div>
+      </main>
     </div>
   );
 };
