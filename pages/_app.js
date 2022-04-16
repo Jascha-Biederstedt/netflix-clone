d40905d5b78188cdfa2ae26119cc3e222dea97b1
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-16 14:52:41 +0200

    Implement app routing depending on login state

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { magic } from '../lib/magic-client';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  const handleCheckLogin = async () => {
+    const isLoggedIn = await magic.user.isLoggedIn();
+
+    if (isLoggedIn) {
+      router.push('/');
+    } else {
+      router.push('/login');
+    }
+  };
+
+  useEffect(() => {
+    handleCheckLogin();
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
