c023c6fa2f1173c7e029c9b9dd454e53fdce68c4
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-17 12:44:25 +0200

    Style video description

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import classNames from 'classnames';
 
 import NavBar from '../../components/navbar/NavBar';
 
@@ -7,6 +8,17 @@ import styles from '../../styles/Video.module.css';
 
 const Video = () => {
   const router = useRouter();
+
+  const video = {
+    title: 'Hi cute dog',
+    publishTime: '1990-01-01',
+    description:
+      'This is the description for the very cute dog Clifford lorem ipsumieuvriue iwueciquiuqbwibiwb weiucbiubcibqcibiqwbcibqwicbiqcb echbuwbczbwucbuzbwzcbzububoqbpbiqwpbuvuibwqekjnb viuiubibjbivbaiusghvisubhewijfThis is the description for the very cute dog Clifford lorem ipsumieuvriue iwueciquiuqbwibiwb weiucbiubcibqcibiqwbcibqwicbiqcb echbuwbczbwucbuzbwzcbzububoqbpbiqwpbuvuibwqekjnb viuiubibjbivbaiusghvisubhewijfThis is the description for the very cute dog Clifford lorem ipsumieuvriue iwueciquiuqbwibiwb weiucbiubcibqcibiqwbcibqwicbiqcb echbuwbczbwucbuzbwzcbzububoqbpbiqwpbuvuibwqekjnb viuiubibjbivbaiusghvisubhewijfThis is the description for the very cute dog Clifford lorem ipsumieuvriue iwueciquiuqbwibiwb weiucbiubcibqcibiqwbcibqwicbiqcb echbuwbczbwucbuzbwzcbzububoqbpbiqwpbuvuibwqekjnb viuiubibjbivbaiusghvisubhewijfThis is the description for the very cute dog Clifford lorem ipsumieuvriue iwueciquiuqbwibiwb weiucbiubcibqcibiqwbcibqwicbiqcb echbuwbczbwucbuzbwzcbzububoqbpbiqwpbuvuibwqekjnb viuiubibjbivbaiusghvisubhewijfThis is the description for the very cute dog Clifford lorem ipsumieuvriue iwueciquiuqbwibiwb weiucbiubcibqcibiqwbcibqwicbiqcb echbuwbczbwucbuzbwzcbzububoqbpbiqwpbuvuibwqekjnb viuiubibjbivbaiusghvisubhewijfThis is the description for the very cute dog Clifford lorem ipsumieuvriue iwueciquiuqbwibiwb weiucbiubcibqcibiqwbcibqwicbiqcb echbuwbczbwucbuzbwzcbzububoqbpbiqwpbuvuibwqekjnb viuiubibjbivbaiusghvisubhewijfThis is the description for the very cute dog Clifford lorem ipsumieuvriue iwueciquiuqbwibiwb weiucbiubcibqcibiqwbcibqwicbiqcb echbuwbczbwucbuzbwzcbzububoqbpbiqwpbuvuibwqekjnb viuiubibjbivbaiusghvisubhewijfThis is the description for the very cute dog Clifford lorem ipsumieuvriue iwueciquiuqbwibiwb weiucbiubcibqcibiqwbcibqwicbiqcb echbuwbczbwucbuzbwzcbzububoqbpbiqwpbuvuibwqekjnb viuiubibjbivbaiusghvisubhewijfThis is the description for the very cute dog Clifford lorem ipsumieuvriue iwueciquiuqbwibiwb weiucbiubcibqcibiqwbcibqwicbiqcb echbuwbczbwucbuzbwzcbzububoqbpbiqwpbuvuibwqekjnb viuiubibjbivbaiusghvisubhewijfThis is the description for the very cute dog Clifford lorem ipsumieuvriue iwueciquiuqbwibiwb weiucbiubcibqcibiqwbcibqwicbiqcb echbuwbczbwucbuzbwzcbzububoqbpbiqwpbuvuibwqekjnb viuiubibjbivbaiusghvisubhewijfThis is the description for the very cute dog Clifford lorem ipsumieuvriue iwueciquiuqbwibiwb weiucbiubcibqcibiqwbcibqwicbiqcb echbuwbczbwucbuzbwzcbzububoqbpbiqwpbuvuibwqekjnb viuiubibjbivbaiusghvisubhewijf',
+    channelTitle: 'Paramount Picture',
+    viewCount: 100000,
+  };
+
+  const { title, publishTime, description, channelTitle, viewCount } = video;
 
   return (
     <div>
@@ -17,11 +29,29 @@ const Video = () => {
           className={styles.videoPlayer}
           id="player"
           type="text/html"
-          width="800"
+          width="99%"
           height="360"
           src={`http://www.youtube.com/embed/${router.query.videoId}?enablejsapi=1&origin=http://example.com&controls=0&rel=1`}
           frameborder="0"
         ></iframe>
+
+        <div className={styles.videoDescription}>
+          <div className={styles.col1}>
+            <p className={styles.publishTime}>{publishTime}</p>
+            <p className={styles.title}>{title}</p>
+            <p className={styles.description}>{description}</p>
+          </div>
+          <div className={styles.col2}>
+            <p className={classNames(styles.subText, styles.subTextWrapper)}>
+              <span className={styles.textColor}>Cast: </span>
+              <span className={styles.channelTitle}>{channelTitle}</span>
+            </p>
+            <p className={styles.subText}>
+              <span className={styles.textColor}>View Count: </span>
+              <span className={styles.viewCount}>{viewCount}</span>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
