bbc08e44b1cecaa0c6236c2f10f3d7ffb731453e
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-17 10:38:42 +0200

    Style the video page

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
+import NavBar from '../../components/navbar/NavBar';
+
+import styles from '../../styles/Video.module.css';
+
 const Video = () => {
   const router = useRouter();
 
   console.log(router);
 
-  return <div>Video {router.query.videoId}</div>;
+  return (
+    <div>
+      <NavBar />
+
+      <div className={styles.container}>
+        <iframe
+          id="player"
+          type="text/html"
+          width="640"
+          height="360"
+          src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+          frameborder="0"
+        ></iframe>
+      </div>
+    </div>
+  );
 };
 
 export default Video;
