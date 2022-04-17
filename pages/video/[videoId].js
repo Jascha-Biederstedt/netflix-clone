f85f9b7ef83df8a7c16b088c7aa8a1823e53d2ef
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-17 10:58:07 +0200

    Style the YouTube player

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -8,19 +8,18 @@ import styles from '../../styles/Video.module.css';
 const Video = () => {
   const router = useRouter();
 
-  console.log(router);
-
   return (
     <div>
       <NavBar />
 
       <div className={styles.container}>
         <iframe
+          className={styles.videoPlayer}
           id="player"
           type="text/html"
-          width="640"
+          width="800"
           height="360"
-          src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+          src={`http://www.youtube.com/embed/${router.query.videoId}?enablejsapi=1&origin=http://example.com&controls=0&rel=1`}
           frameborder="0"
         ></iframe>
       </div>
