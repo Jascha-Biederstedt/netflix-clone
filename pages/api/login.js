c6bfb3924776aee4b887e0c6b47e7c40a5ac1746
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-19 10:43:51 +0200

    Add login API

---
+++ pages/api/login.js
@@ -0,0 +1,14 @@
+const login = async (req, res) => {
+  if (req.method === 'POST') {
+    try {
+      res.send({ done: true });
+    } catch (error) {
+      console.error('Something went wrong logging in', error);
+      res.status(500).send({ done: false });
+    }
+  } else {
+    res.send({ done: false });
+  }
+};
+
+export default login;
