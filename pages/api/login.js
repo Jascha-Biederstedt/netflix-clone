cf9c11bc59d143c7b05c4b9c52f7e6c4ffaf292f
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-04-19 13:46:27 +0200

    Finish login API

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,8 +1,34 @@
 import { magicAdmin } from '../../lib/magic';
+import jwt from 'jsonwebtoken';
 
-const login = async (req, res) => {
+import { isNewUser, createNewUser } from '../../lib/db/hasura';
+import { setTokenCookie } from '../../lib/cookies';
+
+export default async function login(req, res) {
   if (req.method === 'POST') {
     try {
+      const auth = req.headers.authorization;
+      const didToken = auth ? auth.substr(7) : '';
+
+      const metadata = await magicAdmin.users.getMetadataByToken(didToken);
+
+      const token = jwt.sign(
+        {
+          ...metadata,
+          iat: Math.floor(Date.now() / 1000),
+          exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
+          'https://hasura.io/jwt/claims': {
+            'x-hasura-allowed-roles': ['user', 'admin'],
+            'x-hasura-default-role': 'user',
+            'x-hasura-user-id': `${metadata.issuer}`,
+          },
+        },
+        process.env.JWT_SECRET
+      );
+
+      const isNewUserQuery = await isNewUser(token, metadata.issuer);
+      isNewUserQuery && (await createNewUser(token, metadata));
+      setTokenCookie(token, res);
       res.send({ done: true });
     } catch (error) {
       console.error('Something went wrong logging in', error);
@@ -11,6 +37,4 @@ const login = async (req, res) => {
   } else {
     res.send({ done: false });
   }
-};
-
-export default login;
+}
